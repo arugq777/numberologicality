ad5afa01f3ea0d0777e465e337683c426d3dc076
gq <carnsarnit@yahoo.com>
2014-11-03 16:33:13 -0600

    PG version validates; some cosmetic stuff.

--- numberologicality.rb
+++ numberologicality.rb
@@ -7,12 +7,103 @@ require './environments'
 require_relative "lib/letter.rb"
 require_relative "lib/tabulaticate.rb"
 require_relative "lib/person.rb"
-#require "activerecord"
-#require "slim"
-#require "sass"
 
 Mongoid.load!("db/mongoid.yml", :production)
-#class Numberologicality < Sinatra::Base
+
+  set :kindred, {}
+  set :chump, {}
+
+  helpers do 
+    def set_kin(data)
+      settings.kindred = data
+    end
+
+    def post_data(db)
+      if db == :pg
+        settings.chump = process_for_pg(params["nameinput"], params["dateinput"])
+      else
+        settings.chump = process_for_mongo
+      end
+      settings.chump[:ip] = request.ip.to_s + ":" + request.port.to_s
+      settings.chump[:user_agent] = request.user_agent
+      settings.chump[:post_data] = request.POST.to_s
+      save_to_db(settings.chump, db)
+      settings.kindred = find_kindred(settings.chump, db)
+    end
+
+    def find_kindred(data, db)
+      kin = {}
+      if db == :pg
+        [:whole_name, :vowel, :consonant, :birthdate].each do |k|
+          kin[k] = Numnut.where("#{data[k].last} = ANY(#{k.to_s})").limit(10)
+        end
+      elsif db == :mongo
+        [:whole_name, :vowel, :consonant, :birthdate].each do |k|
+          kin[k] = MongoNumnut.where(k => data[k].last).limit(10)
+        end
+      end
+      # puts "kin: #{kin}"
+      kin
+    end
+
+    def process_for_mongo
+      chump = Person.new("","").create_profile(:mongo)
+      data = JSON.parse(request.body.string)
+      data.each do |key, data|
+        chump[key] = data
+      end
+      chump
+    end
+
+    def process_for_pg(name, date)
+      chump = Person.new(name, date).create_profile(:pg)
+      chump
+    end
+
+    def save_to_db(data, db)
+      if db == :pg
+        if Numnut.find_by_name_and_birthday(data.name, data.birthday).nil?
+          data.save!
+        else
+          puts "Not saved."
+        end
+      elsif db == :mongo
+        if MongoNumnut.where(name: data.name, birthday: data.birthday).nil?
+          data.save!
+        else
+          puts "Not saved."
+        end
+      end 
+    end
+
+    def more_results(db)
+      if db == :pg
+        # puts "params['key']: #{params["key"]}"
+        # puts "params['qty']: #{params["qty"]}"
+        # puts "params['number']: #{params["number"]}"
+        more = Numnut.where("#{params['number'].to_s} = ANY(#{params['key'].to_s})")
+                      .offset(settings.kindred[params["key"].to_sym].count)
+                      .limit(params["qty"].to_i)
+        # puts more
+        settings.kindred[params["key"].to_sym] += more
+      elsif db == :mongo
+        data = JSON.parse(request.body.string)
+        more = MongoNumnut.where(data["key"] => data["number"])
+                          .skip(data["index"])
+                          .limit(data["limit"])
+      end
+      more
+    end
+
+    def mongo_query(q)
+
+    end
+
+    def pg_query(q)
+
+    end
+  end
+
   get '/' do
     erb :index
   end
@@ -22,89 +113,29 @@ Mongoid.load!("db/mongoid.yml", :production)
   end
 
   post '/v2' do
-    @chump = Person.new(params[:nameinput], params[:dateinput]).create_profile(:pg)
-    @chump[:ip] = request.ip.to_s + ":" + request.port.to_s
-    @chump[:user_agent] = request.user_agent
-    @chump[:post_data] = request.POST.to_s
-    # [:whole_name, :vowel, :consonant, :birthdate].each do |k|
-    #   puts "chump: #{@chump[k]}"
-    # end
-
-    if Numnut.find_by_name_and_birthday(@chump.name, @chump.birthday).nil?
-      @chump.save!
-    else
-      puts "Not saved."
-    end 
-
-    @kindred = {}
-    [:whole_name, :vowel, :consonant, :birthdate].each do |k|
-      @kindred[k] = Numnut.where("#{@chump[k].last} = ANY(#{k.to_s})").limit(10)
-    end
-    
+    settings.kindred = {}
+    settings.chump = {}
+    post_data(:pg)
     erb :index2
   end
 
+  post '/v2/more' do
+    @more = more_results(:pg)
+    puts "kindred count: #{settings.kindred[params['key'].to_sym].count}"
+    erb :more_names
+  end
 
   post '/' do
-    @chump = Person.new(params[:nameinput], params[:dateinput]).create_profile(:pg)
-    @chump[:ip] = request.ip.to_s + ":" + request.port.to_s
-    @chump[:user_agent] = request.user_agent
-    @chump[:post_data] = request.POST.to_s
-    # [:whole_name, :vowel, :consonant, :birthdate].each do |k|
-    #   puts "chump: #{@chump[k]}"
-    # end
-
-    if Numnut.find_by_name_and_birthday(@chump.name, @chump.birthday).nil?
-      @chump.save!
-    else
-      puts "Not saved."
-    end 
-
-    @kindred = {}
-    [:whole_name, :vowel, :consonant, :birthdate].each do |k|
-      @kindred[k] = Numnut.where("#{@chump[k].last} = ANY(#{k.to_s})").limit(10)
-    end
-
-    # @sample_kin = {}
-    # 10.times do
-    #   [:whole_name, :vowel, :consonant, :birthdate].each do |k|
-    #     @sample_kin[k] << @kindred[k].sample
-    #   end
-    # end
-
+    post_data(:pg)
     erb :index
   end
 
   post "/json/more" do
-    # puts "/more JSON parse: #{JSON.parse(request.body.string)}"
-    data = JSON.parse(request.body.string)
-    @more = MongoNumnut.where(data["key"] => data["number"])
-                       .skip(data["index"])
-                       .limit(data["limit"])
+    @more = more_results(:mongo)
     @more.to_json
   end
 
   post '/json' do
-    # params.each { |k,v| puts "params: #{k}, #{v}"}
-    # puts "request params: #{request.params}"
-    # puts "JSON parse: #{JSON.parse(request.body.string)}"
-    data = JSON.parse(request.body.string)
-    @chump = Person.new("","").create_profile(:mongo)
-    data.each do |key, data|
-      @chump[key] = data
-    end
-    @chump[:ip] = request.ip.to_s + ":" + request.port.to_s
-    @chump[:user_agent] = request.user_agent
-    @chump[:post_data] = request.POST.to_s
-    @chump.save!
-    # data.each_key do |k|
-    #   puts "chump #{k}: #{@chump[k]}"
-    # end
-    @kindred = {}
-    [:whole_name, :vowel, :consonant, :birthdate].each do |k|
-      @kindred[k] = MongoNumnut.where(k => @chump[k].last).limit(10)
-    end
-    @kindred.to_json
+    post_data(:mongo)
+    settings.kindred.to_json
   end
-#end
-
--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,49 @@
-// Declare app level module which depends on filters, and services
-angular.module('numberologicality', ['ngResource', 'ngRoute', 'ui.bootstrap', 'ui.date'])
-  .config(['$routeProvider', function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/home/home.html', 
-        controller: 'HomeController'})
-      .otherwise({redirectTo: '/'});
-  }]);
+var numNames = [".whole_name", ".consonant", ".vowel", ".birthdate"];
+
+$(document).ready( function(){
+  // for (var i = numNames.length - 1; i >= 0; i--) {
+  //   $("form"+numNames[i]).submit( function(ev){
+  //     ev.preventDefault();
+  //     getMoreNames(ev, $("form"+numNames[i]), numNames[i] );
+  //   });
+  // };
+  $("form.whole_name").submit( function(ev){
+    ev.preventDefault();
+    getMoreNames(ev, $("form.whole_name"), ".whole_name" );
+  });
+  $("form.consonant").submit( function(ev){
+    ev.preventDefault();
+    getMoreNames(ev, $("form.consonant"), ".consonant"  );
+  });
+  $("form.vowel").submit( function(ev){
+    ev.preventDefault();
+    getMoreNames(ev, $("form.vowel"), ".vowel" );
+  });
+  $("form.birthdate").submit( function(ev){
+    ev.preventDefault();
+    getMoreNames(ev, $("form.birthdate"), ".birthdate" );
+  });
+
+});
+
+function getMoreNames(ev, form, name){
+  console.log(form);
+  $.ajax({
+    timeout:3000,
+    type: form.attr('method'),
+    url: form.attr('action'),
+    data: form.serialize(),
+    dataType: 'html',
+    success: function(data){
+      console.log(data);
+      ev.preventDefault();
+      $(data).appendTo("ul.names" + name);
+    },
+    error: function() {
+      //error();
+    },
+    complete: function(){
+      //updateInfo();
+    }
+  });
+}
